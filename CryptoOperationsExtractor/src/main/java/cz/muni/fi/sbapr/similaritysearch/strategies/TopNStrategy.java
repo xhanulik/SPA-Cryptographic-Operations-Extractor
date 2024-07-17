10f129dd5cf1ec2d2aa5e81bf24599b964317ecb
Veronika Hanulíková <xhanulik@fi.muni.cz>
2024-07-17 18:38:05 +0200

    Print out 24 best similarities

--- CryptoOperationsExtractor/src/main/java/cz/muni/fi/sbapr/similaritysearch/strategies/TopNStrategy.java
+++ CryptoOperationsExtractor/src/main/java/cz/muni/fi/sbapr/similaritysearch/strategies/TopNStrategy.java
@@ -15,7 +15,7 @@ import cz.muni.fi.sbapr.util.OverlappingPair;
  * @author Martin
  */
 public class TopNStrategy implements SimilaritySearchStrategy {
-    public static final int TOP_N_STRATEGY_COUNT = 3;
+    public static final int TOP_N_STRATEGY_COUNT = 24;
     
     private final SortedSet<Similarity> similarities;
     private final int topNOccurences;
